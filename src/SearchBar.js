c9993994b2be5b19a34099a166b9f13d26846935
Charlie Axelrod <charlieaxelrod@gmail.com>
2019-01-15 16:34:36 -0800

    Creates search bar form

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,14 +1,28 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+  }
   
   render() {
     return (
-      <div>
-        {this.props.language === 'en'
-        ? <div>searchbar</div>
-        : <div>barra de pesquisa</div>}
-      </div>
+      <form onSubmit={this.handleSubmit}>
+        <input type="text" value={this.state.value} onChange={this.handleChange} />
+        <input type="submit" value={this.props.language === 'en' ? 'Search' : 'Pesquisar'} />
+      </form>
     );
   }
 }
